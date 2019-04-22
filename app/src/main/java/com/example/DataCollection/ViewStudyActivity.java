b5d7faa82b1cfa5d31097d23f8ff6de27f0213c5
rsvagle <rsvagle@gmail.com>
2019-04-22 15:47:12 -0500

    Got it to pull the state file.json from assets properly and made updates to get data passed between activities.

--- app/src/main/java/com/example/DataCollection/ViewStudyActivity.java
+++ app/src/main/java/com/example/DataCollection/ViewStudyActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ViewStudyActivity extends AppCompatActivity {
 
@@ -19,6 +22,10 @@ public class ViewStudyActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: Started.");
         currentStudy = (Study) getIntent().getSerializableExtra("study");
 
+        TextView studyName = findViewById(R.id.study_name_textView);
+        studyName.setText(currentStudy.getStudyName());
+        TextView studyId = findViewById(R.id.study_id_tv);
+        studyId.setText("ID: " + currentStudy.getStudyID());
         Button siteAddReadingsButton = findViewById(R.id.add_readings_btn);
         Button exportStudyButton = findViewById(R.id.export_study_btn);
         Button addSiteButton = findViewById(R.id.add_site_btn);
