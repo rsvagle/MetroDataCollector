d5fb583c8b1e9e0b618edf1453d103224d0e5ea7
Tyler C <47093114+unholypond@users.noreply.github.com>
2019-04-26 13:12:59 -0500

    Comments added
    Comments added to IBehavior, IReader and IReaderFactory classes.

--- app/src/main/java/com/example/DataCollection/IBehavior.java
+++ app/src/main/java/com/example/DataCollection/IBehavior.java
@@ -2,6 +2,14 @@ package com.example.DataCollection;
 
 import java.util.ArrayList;
 
+/**
+ * This method builds Item array and sets the study. 
+ * @param void
+ * 
+ * @return
+ * void
+ */
+
 public interface IBehavior {
 
     ArrayList<Item> getItems();
